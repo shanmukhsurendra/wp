2d7b5e0d288d6d3d09a9b0baa18180067de2b71c
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2019-01-25 12:38:38 +0530

    exam2

--- wpexam2/script.js
+++ wpexam2/script.js
@@ -1,23 +1,24 @@
 var x = 0;
-var password = "password";
+// var password = "pa";
 var array = Array();
 var array1 = Array();
+var p = "pa";
 
 function add_element_to_array()
 {
 	var a = document.getElementById("pwd").value;
 	var b = document.getElementById("usr").value;
-	var p = "password";
 	var op = document.getElementById("pwd").value;
-	if(a=="" || b=="")
+	var c = document.getElementById("text1").value;
+	if(a=="" || b=="" || c== "")
 	{
 	alert("Please fill all the boxes");
 	return ;
 	}
 	if(op != p) {
 		alert("wrong password");
-		document.getElementById("text1").value = "";
-		document.getElementById("usr").value = "";
+		// document.getElementById("text1").value = "";
+		// document.getElementById("usr").value = "";
 		document.getElementById("pwd").value = "";
 		return ;
 	}
@@ -30,14 +31,14 @@ function add_element_to_array()
  document.getElementById("pwd").value = "";
  	
    var e = "<hr/>";
-   var f = "<hr/>";  
+   // var f = "<hr/>";  
     
    for (var y=array.length-1; y>=0; y--)
    {
-     e +=  array[y] + "<br/>" ;
-     f += + array1[y] + "<br/>";
+     e +=  "<p>" + array[y] + "</p>" + "commented by --> " +array1[y] + "<br/>"+ "<br/>";
+     // f += +  + "<br/>";
    }
    document.getElementById("Result").innerHTML = e;
-   document.getElementById("name").innerHTML = f;
+   // document.getElementById("name").innerHTML = f;
  }
 
